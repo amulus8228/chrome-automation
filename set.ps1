ccce65b4fcf287860701453128ea8938c0f7db8f
amulus8228 <148557532+amulus8228@users.noreply.github.com>
2023-10-22 17:43:49 +0530

    Update set.ps1

--- set.ps1
+++ set.ps1
@@ -1,3 +1,6 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+git config --global user.email "pranavdileep10@gmail.com"
+git config --global user.name "PRANAV K DILEEP"
+choco install androidstudio -y
